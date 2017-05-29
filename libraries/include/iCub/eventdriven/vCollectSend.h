541b35940d16465043de897a297e7f9fde202043
icub <icub@iit.it>
2017-05-29 17:28:23 +0200

    added a check on the bottle -now we write only if the yarpstamp has changed

--- libraries/include/iCub/eventdriven/vCollectSend.h
+++ libraries/include/iCub/eventdriven/vCollectSend.h
@@ -3,7 +3,12 @@
 
 #include <iCub/eventdriven/vCodec.h>
 #include <iCub/eventdriven/vBottle.h>
+#include <iCub/eventdriven/vtsHelper.h>
 #include <yarp/os/all.h>
+#include <limits>
+#include <iomanip>
+
+typedef std::numeric_limits< double > dbl;
 
 namespace ev {
 
@@ -15,6 +20,8 @@ private:
     yarp::os::BufferedPort<vBottle> sendPort;
     yarp::os::Mutex m;
     yarp::os::Stamp ystamp;
+    double currystamp;
+    double prevystamp;
     int prevstamp;
 
 
@@ -23,6 +30,7 @@ public:
     collectorPort() : RateThread(1.0) {
 
         prevstamp = 0;
+        prevystamp = 0.0;
 
     }
 
@@ -36,11 +44,12 @@ public:
 
         m.lock();
 //        if((v->stamp - prevstamp) < 0)
-//            std::cout << v->stamp << std::endl;
+//            std::cout << prevstamp * vtsHelper::tsscaler << " " << v->stamp * vtsHelper::tsscaler << std::endl;
         prevstamp = v->stamp;
 
         filler.addEvent(v);
         ystamp = y;
+        currystamp = ystamp.getTime();
         m.unlock();
 
     }
@@ -54,11 +63,15 @@ public:
             b = filler;
             filler.clear();
             sendPort.setEnvelope(ystamp);
+//            std::cout.precision( dbl::digits10 );
+//            std::cout << (currystamp - prevystamp) << std::endl;
+            prevystamp = currystamp;
             m.unlock();
 
         }
 
-        sendPort.write();
+         if(!(currystamp - prevystamp))
+             sendPort.write();
 
     }
 
